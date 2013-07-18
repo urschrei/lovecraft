c7487e489946d6366f93c0c7850c7660adacc1c6
Stephan Hügel <stephan.hugel.12@ucl.ac.uk>
2013-07-18 15:26:59 +0100

    Only count noun frequency

--- counts.py
+++ counts.py
@@ -12,9 +12,15 @@ fd = nltk.FreqDist()
 # read in the corpus, remove punctuation, and convert it all to lowercase
 # this is slightly slower than reading the entire file into memory
 # but it's more memory-efficient by far
+# we want to keep the dash/minus, since en and em aren't used in the text
+punc = [char for char in string.punctuation]
+punc.remove('-')
 with open("lovecraft.txt", 'r') as f:
     for line in f:
-        inp = line.translate(None, string.punctuation).lower()
+        inp = line.translate(None, ''.join(punc)).lower().decode('utf8')
         for sent in nltk.sent_tokenize(inp):
-            for word in nltk.word_tokenize(sent):
-                fd.inc(word)
+            tagged = nltk.pos_tag(nltk.word_tokenize(sent))
+            # we really only want nouns
+            nouns = [word for word in tagged if word[1] == 'NN']
+            if nouns:
+                [fd.inc(noun[0].encode('utf8')) for noun in nouns]
